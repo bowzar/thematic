f551be2e7b98752933ebced37cd62c9fb2442cdd
HIDE <bowzar@qq.com>
2018-03-13 21:49:25 +0800

    更新框架

---
+++ thematic-example/src/test/java/com/yulintu/thematic/example/redis/TestRedis.java
@@ -0,0 +1,36 @@
+package com.yulintu.thematic.example.redis;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import redis.clients.jedis.Jedis;
+
+import java.util.Set;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class TestRedis {
+
+    private Jedis jedis;
+
+    @Before
+    public void before() {
+        jedis = new Jedis("localhost");
+        jedis.auth("123456");
+    }
+
+    @Test
+    public void testCommon() {
+        System.out.println("服务正在运行: " + jedis.ping());
+    }
+
+    @Test
+    public void testKeys() {
+        Set<String> keys = jedis.keys("*");
+
+        jedis.set("name", "JY");
+        keys.stream().forEach(c -> System.out.println(c));
+        System.out.println(jedis.get("name"));
+    }
+}
