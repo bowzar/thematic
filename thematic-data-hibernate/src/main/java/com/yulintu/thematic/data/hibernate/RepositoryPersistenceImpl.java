6c7a2f5ca3aa337670ef17262f7e86148bcd3aea
HIDE <bowzar@qq.com>
2018-04-12 17:47:11 +0800

    update

--- thematic-data-hibernate/src/main/java/com/yulintu/thematic/data/hibernate/RepositoryPersistenceImpl.java
+++ thematic-data-hibernate/src/main/java/com/yulintu/thematic/data/hibernate/RepositoryPersistenceImpl.java
@@ -1,8 +1,14 @@
 package com.yulintu.thematic.data.hibernate;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.querydsl.sql.Configuration;
+import com.querydsl.sql.SQLQueryFactory;
 import com.yulintu.thematic.data.Provider;
 import com.yulintu.thematic.data.Repository;
 import com.yulintu.thematic.data.RepositoryImpl;
+import org.hibernate.Session;
+
+import javax.persistence.EntityManager;
 
 public class RepositoryPersistenceImpl extends RepositoryImpl implements Repository {
 
@@ -15,6 +21,22 @@ public class RepositoryPersistenceImpl extends RepositoryImpl implements Reposit
     //region methods
     public ProviderPersistence getPersistenceProvider() {
         return (ProviderPersistence) getProvider();
+    }
+
+    protected EntityManager getEntityManager(){
+        return getPersistenceProvider().getCurrentEntityManager();
+    }
+
+    protected Session getSession(){
+        return getPersistenceProvider().getCurrentSession();
+    }
+
+    protected JPAQueryFactory getJPAQueryFactory(){
+        return getPersistenceProvider().getCurrentJPAQueryFactory();
+    }
+
+    protected SQLQueryFactory getSQLQueryFactory(Configuration config){
+        return getPersistenceProvider().getCurrentSQLQueryFactory(config);
     }
     //endregion
 }
