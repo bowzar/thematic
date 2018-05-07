1af88a3b386eaaf04bf4aeb47e3bc3a6872430fe
HIDE <bowzar@qq.com>
2018-05-07 11:29:33 +0800

    update

--- thematic-data-sharding/src/main/java/com/yulintu/thematic/data/sharding/ShardReducer.java
+++ thematic-data-sharding/src/main/java/com/yulintu/thematic/data/sharding/ShardReducer.java
@@ -2,7 +2,5 @@ package com.yulintu.thematic.data.sharding;
 
 public interface ShardReducer {
 
-
-
     public Object reduce(ShardMethodInvokeMetadata metadata, ShardMethodInvokeResult[] shards);
 }
